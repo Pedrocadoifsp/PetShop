e5de02c2892711d4b0232114a9e881b55ed99794
Pedro Augusto Oliveira da Silva <pedroaugustoo139@gmail.com>
2026-03-25 08:38:20 -0300

    Projeto finalizado

--- src/server.ts
+++ src/server.ts
@@ -17,7 +17,7 @@ server.use(express.static(path.join(__dirname, '../public')));
 server.use(mainRouter);
 
 server.use((req, res) => {
-	res.send('404 - Not Found');
+	res.render('404 - Not Found');
 });
 
 server.listen(process.env.PORT);
